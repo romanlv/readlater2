9f5e7c5ff03f576f7046a2146f0da394e21167fe
roman <r@vie.dev>
2025-10-31 23:52:50 -0400

    update manifest for pwa

--- packages/app/vite.config.ts
+++ packages/app/vite.config.ts
@@ -27,21 +27,22 @@ export default mergeConfig(defineConfig({
       background_color: '#ffffff',
       display: 'standalone',
       start_url: basePath,
+      prefer_related_applications: false,
 
       icons: [{
-        src: 'pwa-64x64.png',
+        src: `${basePath}pwa-64x64.png`,
         sizes: '64x64',
         type: 'image/png',
       }, {
-        src: 'pwa-192x192.png',
+        src: `${basePath}pwa-192x192.png`,
         sizes: '192x192',
         type: 'image/png',
       }, {
-        src: 'pwa-512x512.png',
+        src: `${basePath}pwa-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
       }, {
-        src: 'maskable-icon-512x512.png',
+        src: `${basePath}maskable-icon-512x512.png`,
         sizes: '512x512',
         type: 'image/png',
         purpose: 'maskable',
